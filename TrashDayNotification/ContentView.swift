bd2e7da9b6af0cb7786105874aff69eb9cce70f7
Kazu <nokaha1222@gmail.com>
2022-06-11 20:55:34 +0900

    Initial Commit

---
+++ TrashDayNotification/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrashDayNotification
+//
+//  Created by 能勢航羽 on 2022/06/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrashDayNotification/TrashDayNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrashDayNotificationApp.swift
+//  TrashDayNotification
+//
+//  Created by 能勢航羽 on 2022/06/11.
+//
+
+import SwiftUI
+
+@main
+struct TrashDayNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
